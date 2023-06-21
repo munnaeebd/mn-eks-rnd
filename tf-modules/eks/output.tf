fe37361e3e54366fa5ad4732dc9165423bf40fb2
mohammad-tawhid <mohammad.tawhid@bkash.com>
2023-06-21 17:17:20 +0600

    ok

--- tf-modules/eks/output.tf
+++ tf-modules/eks/output.tf
@@ -4,9 +4,6 @@ output "eks_endpoint" {
 output "eks_arn" {
   value = aws_eks_cluster.eks.arn
 }
-output "eks_arn" {
-  value = aws_eks_cluster.eks.arn
-}
 output "eks_certificate_authority" {
   value = aws_eks_cluster.eks.certificate_authority
 }
